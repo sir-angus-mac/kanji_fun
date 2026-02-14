45e0567228f1837479e9ad00ec39998f349c44f2
sir-angus-mac <angusmak0324@gmail.com>
2026-02-15 02:06:30 +0900

    Add kana.js attempt with LFS tracking

--- ground.js
+++ ground.js
@@ -1,19 +1,3 @@
-class Ground {
-  constructor(x, y, w, h) {
-    this.w = w;
-    this.h = h;
-    
-    this.body = Bodies.rectangle(x, y, this.w, this.h, {isStatic: true});
-    Composite.add(engine.world, this.body);
-  }
-  
-  display() {
-    push();
-    rectMode(CENTER);
-    let x = this.body.position.x;
-    let y = this.body.position.y; 
-    translate(x, y);
-    rect(0, 0, this.w, this.h);
-    pop();
-  }
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:9ec9e2b1cfc6a14a10138decd588a77bd969891d3d890b4ccdd25c351c8d9a43
+size 409
